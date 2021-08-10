1ea1ec4ade0262ab34d4f586268761ece0650100
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-10 23:28:39 +0530

    Update `node-webpmux` typings

--- src/lib/node-webmux.d.ts
+++ src/lib/node-webmux.d.ts
@@ -2,7 +2,7 @@ declare module 'node-webpmux' {
     export class Image {
         constructor()
         exif: Buffer
-        loadBuffer(buffer: Bufffer)
-        saveBuffer(): Promise<Buffer>
+        load(buffer: Bufffer | string): Promise<void>
+        save(...args: unknown): Promise<Buffer>
     }
 }
