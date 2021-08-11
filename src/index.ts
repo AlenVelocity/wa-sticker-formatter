9c730d21a4868510a3706d105cf9439da6f93474
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-11 15:43:33 +0530

    add Exports

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { Sticker } from './Sticker'
+
+export * from './Sticker'
+export * from './Types'
+export { default as StickerMetadata } from './lib/Metadata/StickerMetadata'
+export default Sticker
