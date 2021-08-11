9e3bd7364bf008fe3ac83e8b9b9d8c2bf135e5dc
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-11 21:41:44 +0530

    Update Example

--- examples/index.mjs
+++ examples/index.mjs
@@ -1,16 +1,23 @@
-import { writeFileSync } from 'fs'
+import { statSync, writeFileSync } from 'fs'
 import { Sticker } from '../dist/Sticker.js'
 
-const image = 'https://c.tenor.com/WZBvSgw5JMgAAAAC/watson-amelia-amelia-watson.gif'
-new Sticker(image).build().then((data) => writeFileSync('sticker_default.webp', data))
-new Sticker(image, {
-    type: 'crop'
-}).build().then((data) => writeFileSync('sticker_crop.webp', data))
+const main = async () => {
+    const image = 'https://c.tenor.com/WZBvSgw5JMgAAAAC/watson-amelia-amelia-watson.gif'
+    await new Sticker(image).build().then((data) => writeFileSync('sticker_default.webp', data))
+    console.log('default', statSync('sticker_default.webp').size)
+    await new Sticker(image, {
+        type: 'crop'
+    }).build().then((data) => writeFileSync('sticker_crop.webp', data))
+    console.log('crop', statSync('sticker_crop.webp').size)
 
-new Sticker(image, {
-    type: 'full',
-    pack: 'ship',
-    categories: [
-        '🌹'
-    ]
-}).build().then((data) => writeFileSync('sticker_full.webp', data))
+    await new Sticker(image, {
+        type: 'full',
+        pack: 'ship',
+        categories: [
+            '🌹'
+        ]
+    }).build().then((data) => writeFileSync('sticker_full.webp', data))
+    console.log('full', statSync('sticker_full.webp').size)
+}
+
+main()
