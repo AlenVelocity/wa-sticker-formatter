e7e96ea73d2e0ec7404e67173fd3532dd192218e
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-11 15:40:05 +0530

    Update methods

--- src/lib/videoToGif.ts
+++ src/lib/videoToGif.ts
@@ -3,12 +3,12 @@ import { writeFile, readFile, unlink } from 'fs/promises'
 import { tmpdir } from 'os'
 
 /** https://stackoverflow.com/questions/52156713/fluent-ffmpeg-h264-to-gif-throwing-error-1 */
-const videoToGif = async (data: Buffer) => {
+const videoToGif = async (data: Buffer): Promise<Buffer> => {
     const filename = `${tmpdir()}/${Math.random().toString(36)}`
     const [video, gif] = ['video', 'gif'].map((ext) => `${filename}.${ext}`)
     await writeFile(video, data)
     await new Promise((resolve) => {
-        ffmpeg(video).outputOption('-vf', 'scale=320:-1:flags=lanczos,fps=15').save(gif).on('end', resolve)
+        ffmpeg(video).save(gif).on('end', resolve)
     })
     const buffer = await readFile(gif)
     ;[video, gif].forEach((file) => unlink(file))
