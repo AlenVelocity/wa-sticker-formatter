f672fcc2fb49aca19cc3a6777ef77655aa198804
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-11 15:38:47 +0530

    Update `RawMetadata`

--- src/lib/Metadata/RawMetadata.ts
+++ src/lib/Metadata/RawMetadata.ts
@@ -1,4 +1,5 @@
 import { IRawMetadata, Metadata } from '../../Types'
+import Utils from '../../Utils'
 
 export default class RawMetadata implements IRawMetadata {
     emojis: string[]
@@ -6,9 +7,9 @@ export default class RawMetadata implements IRawMetadata {
     'sticker-pack-name': string
     'sticker-pack-publisher': string
     constructor(options: Metadata) {
-        this['sticker-pack-id'] = options.id
-        this['sticker-pack-name'] = options.pack
-        this['sticker-pack-publisher'] = options.author
-        this.emojis = options.categories
+        this['sticker-pack-id'] = options.id || Utils.generateStickerID()
+        this['sticker-pack-name'] = options.pack || ''
+        this['sticker-pack-publisher'] = options.author || ''
+        this.emojis = options.categories || []
     }
 }
