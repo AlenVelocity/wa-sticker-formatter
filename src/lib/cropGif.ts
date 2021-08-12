8933612947b121875ff6c63bebb67f69adcd412c
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-12 11:18:46 +0530

    add function to cro GIFs and convert to WebP

---
+++ src/lib/cropGif.ts
@@ -0,0 +1,32 @@
+import Ffmpeg from 'fluent-ffmpeg'
+import { readFile } from 'fs/promises'
+import { tmpdir } from 'os'
+
+const cropGif = async (filename: string): Promise<Buffer> => {
+    const file = await new Promise<string>((resolve) => {
+        const name = `${tmpdir()}/${Math.random().toString(36)}.webp`
+        Ffmpeg(filename)
+            // eslint-disable-next-line no-useless-escape
+            .outputOptions([
+                '-vcodec',
+                'libwebp',
+                '-vf',
+                // eslint-disable-next-line no-useless-escape
+                `crop=w='min(min(iw\,ih)\,500)':h='min(min(iw\,ih)\,500)',scale=500:500,setsar=1,fps=15`,
+                '-loop',
+                '0',
+                '-preset',
+                'default',
+                '-an',
+                '-vsync',
+                '0',
+                '-s',
+                '512:512'
+            ])
+            .save(name)
+            .on('end', () => resolve(name))
+    })
+    return await readFile(file)
+}
+
+export default cropGif
