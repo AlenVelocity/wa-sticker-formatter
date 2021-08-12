3ff5aea3b4b733516d4bd40530cf097077816558
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-12 15:16:53 +0530

    update video convert method

--- src/lib/videoToGif.ts
+++ src/lib/videoToGif.ts
@@ -8,8 +8,10 @@ const videoToGif = async (data: Buffer): Promise<Buffer> => {
     const [video, gif] = ['video', 'gif'].map((ext) => `${filename}.${ext}`)
     await writeFile(video, data)
     await new Promise((resolve) => {
-        ffmpeg(video).save(gif).on('end', resolve)
+        ffmpeg(video)
+        .save(gif).on('end', resolve)
     })
+
     const buffer = await readFile(gif)
     ;[video, gif].forEach((file) => unlink(file))
     return buffer
