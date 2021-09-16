c7f68ea33511dfb69f17ffc21e710ee08538843f
alensaito1 <erwinsmithscout1@gmail.com>
2021-09-16 17:42:55 +0530

    Update convert

--- src/lib/convert.ts
+++ src/lib/convert.ts
@@ -1,14 +1,19 @@
 import sharp from 'sharp'
 import videoToGif from './videoToGif'
-import sizeOf from 'image-size'
 import { writeFile } from 'fs/promises'
 import { tmpdir } from 'os'
 import crop from './crop'
 
-const convert = async (data: Buffer, mime: string, type: 'crop' | 'full' | 'default' = 'default'): Promise<Buffer> => {
+const convert = async (
+    data: Buffer,
+    mime: string,
+    type: 'crop' | 'full' | 'default' = 'default',
+    quality = 100
+): Promise<Buffer> => {
     const isVideo = mime.startsWith('video')
     const image = isVideo ? await videoToGif(data) : data
-    if ((isVideo || mime.includes('gif')) && type === 'crop') {
+    const isAnimated = isVideo || mime.includes('gif')
+    if (isAnimated && type === 'crop') {
         const filename = `${tmpdir()}/${Math.random().toString(36)}.webp`
         await writeFile(filename, image)
         return convert(await crop(filename), 'image/webp', 'default')
@@ -19,30 +24,37 @@ const convert = async (data: Buffer, mime: string, type: 'crop' | 'full' | 'defa
     if (type === 'crop') img.resize(512, 512, { fit: 'contain' })
 
     if (type === 'full') {
-        let { height, width } = sizeOf(image)
-        ;[height, width] = [height, width].map((x) => x || 0)
-        const [sub, compare] = [Math.abs(height - width), height > width]
-        const options = ((): sharp.ExtendOptions => {
-            const [horizontal, vertical] = [compare ? sub : 0, !compare ? sub : 0].map((number) =>
-                Math.round(number / 2)
-            )
-            return {
-                top: vertical,
-                left: horizontal,
-                right: horizontal,
-                bottom: vertical,
-                background: {
-                    r: 255,
-                    g: 255,
-                    b: 255,
-                    alpha: 0
-                }
-            }
-        })()
-        img.extend(options)
+        const { height = 0, width = 0, pages = 1 } = await img.metadata()
+        const pageHeight = height / pages
+        if (height !== width) {
+            const [sub, compare] = [Math.abs(pageHeight - width), pageHeight > width]
+            const [horizontal, vertical] = [compare ? sub : 0, compare ? 0 : sub].map((x) => Math.round(x / 2))
+            return await img
+                .webp({
+                    quality,
+                    pageHeight: Math.max(pageHeight, width)
+                })
+                .extend({
+                    top: vertical,
+                    left: horizontal,
+                    right: horizontal,
+                    bottom: vertical,
+                    background: {
+                        r: 255,
+                        g: 255,
+                        b: 255,
+                        alpha: 0
+                    }
+                })
+                .toBuffer()
+        }
     }
 
-    return await img.toBuffer()
+    return await img
+        .webp({
+            quality
+        })
+        .toBuffer()
 }
 
 export default convert
