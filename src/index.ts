f45feb30edfe46279b57edd303d708fc8d9608dc
alensaito1 <erwinsmithscout1@gmail.com>
2021-09-16 17:43:04 +0530

    export `Exif` class

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import { Sticker } from './Sticker'
 export * from './Sticker'
 export * from './Types'
 export { default as StickerMetadata } from './lib/Metadata/StickerMetadata'
+export { default as Exif } from './lib/Metadata/Exif'
 export default Sticker
