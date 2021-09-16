f1dccce95a48f58050298b4e9491745393166993
alensaito1 <erwinsmithscout1@gmail.com>
2021-09-16 18:25:09 +0530

    unit tests with `mocha`

---
+++ tests/Sticker.test.ts
@@ -0,0 +1,90 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path='../src/lib/node-webmux' />
+import { strict as assert } from 'assert'
+import Sticker from '../src'
+import { Image } from 'node-webpmux'
+import sizeOf from 'image-size'
+
+const images = {
+    static: {
+        potrait: 'https://i.pinimg.com/originals/3a/53/d6/3a53d68345b56241a875595b21ec2a59.jpg',
+        landscape: 'https://chasinganime.com/wp-content/uploads/2021/02/0_YgtEypuJ2QfMPCbn.jpg'
+    },
+    animated: {
+        potrait: 'hhttps://c.tenor.com/-1mtmQgH5eYAAAAC/watson-amelia-vtuber.gif',
+        landscape: 'https://c.tenor.com/2RdLoyV5VPsAAAAC/ayame-nakiri.gif'
+    }
+}
+
+describe('Sticker', () => {
+
+    describe('Static Stickers', () => {
+        it('should create a static sticker', async () => {
+            const sticker = new Sticker(images.static.potrait)
+            const buffer = await sticker.build()
+            assert.ok(buffer)
+        })
+
+        it('should create a cropped sticker with size 512x512', async () => {
+            const sticker = new Sticker(images.static.potrait, {
+                type: 'crop'
+            })
+            const buffer = await sticker.build()
+            const { height, width } = sizeOf(buffer)
+            assert.equal(height, 512)
+            assert.equal(width, 512)
+        })
+
+        it('should create a static sticker same height and width', async () => {
+            const sticker = new Sticker(images.static.landscape, {
+                type: 'full'
+            })
+            const buffer = await sticker.build()
+            const { height, width } = sizeOf(buffer)
+            assert.equal(height, width)
+        })
+    })
+
+    describe('Animated Stickers', () => {
+        it('should create an animated sticker', async () => {
+            const sticker = new Sticker(images.animated.landscape)
+            const buffer = await sticker.build()
+            assert.ok(buffer)
+        })
+
+        it('should create an animated sticker with size 512x512', async () => {
+            const sticker = new Sticker(images.animated.potrait, {
+                type: 'crop'
+            })
+            const buffer = await sticker.build()
+            const { height, width } = sizeOf(buffer)
+            assert.equal(height, 512)
+            assert.equal(width, 512)
+        })
+
+        it('should create an animated sticker same height and width', async () => {
+            const sticker = new Sticker(images.animated.potrait, {
+                type: 'full'
+            })
+            const buffer = await sticker.build()
+            const { height, width } = sizeOf(buffer)
+            assert.equal(height, width)
+        })
+    })
+    
+    describe('Metadata', () => {
+        it('should create sticker with the provided pack and author name', async () => {
+            const sticker = new Sticker(images.static.potrait, {
+                pack: 'WSF',
+                author: 'Well'
+            })
+            const buffer = await sticker.build()
+            const image = new Image()
+            await image.load(buffer)
+            const exif = image.exif.toString('utf-8')
+            assert.equal(exif.includes('WSF'), true)
+            assert.equal(exif.includes('Well'), true)
+        })
+    })
+})
+
