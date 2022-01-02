ebdded81d609ed6af13acc849d6effe397354bb1
alensaito1 <erwinsmithscout1@gmail.com>
2022-01-02 10:59:00 +0530

    fix(): extractMetadata throwing error #33

--- src/extractMetadata.ts
+++ src/extractMetadata.ts
@@ -8,6 +8,6 @@ import { IRawMetadata } from '.'
 export const extractMetadata = async (image: Buffer): Promise<Partial<IRawMetadata>> => {
     const img = new Image()
     await img.load(image)
-    const exif = img.exif.toString('utf-8')
+    const exif = img.exif?.toString('utf-8') ?? '{}'
     return JSON.parse(exif.substring(exif.indexOf('{'), exif.lastIndexOf('}') + 1) ?? '{}') as IRawMetadata
 }
