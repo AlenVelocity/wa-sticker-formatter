ac5577a5a0eba438e9d3453cde532d5aa7568018
alensaito1 <erwinsmithscout1@gmail.com>
2022-01-02 10:53:04 +0530

    feat(): circle type #32

--- src/internal/convert.ts
+++ src/internal/convert.ts
@@ -1,4 +1,4 @@
-import sharp from 'sharp'
+import sharp, { fit } from 'sharp'
 import videoToGif from './videoToGif'
 import { writeFile } from 'fs-extra'
 import { tmpdir } from 'os'
@@ -16,24 +16,36 @@ const convert = async (
     let image = isVideo ? await videoToGif(data) : data
     const isAnimated = isVideo || mime.includes('gif')
 
-    if (isAnimated && type === 'crop') {
+    if (isAnimated && ['crop', 'circle'].includes(type)) {
         const filename = `${tmpdir()}/${Math.random().toString(36)}.webp`
         await writeFile(filename, image)
-        ;[image, type] = [await crop(filename), StickerTypes.DEFAULT]
+        ;[image, type] = [await crop(filename), type === 'circle' ? StickerTypes.CIRCLE : StickerTypes.DEFAULT]
     }
 
-    const img = sharp(image, { animated: true }).toFormat('webp')
+    const img = sharp(image, { animated: type !== 'circle' }).toFormat('webp')
 
     if (type === 'crop')
         img.resize(512, 512, {
-            fit: 'cover'
+            fit: fit.cover
         })
 
-    if (type === 'full') {
+    if (type === 'full')
         img.resize(512, 512, {
-            fit: 'contain',
+            fit: fit.contain,
             background
         })
+
+    if (type === 'circle') {
+        img.resize(512, 512, {
+            fit: fit.cover
+        }).composite([
+            {
+                input: Buffer.from(
+                    `<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><circle cx="256" cy="256" r="256" fill="${background}"/></svg>`
+                ),
+                blend: 'dest-in'
+            }
+        ])
     }
 
     return await img
